5f4699b49e7cde9886620fe7b38ff543a8fcb2a5
Sebastian-Fierro <sebastian.fierro2101@alumnos.ubiobio.cl>
2025-10-28 17:28:31 +0000

    usuario weno

--- 28-10/firebase/src/components/Usuario.jsx
+++ 28-10/firebase/src/components/Usuario.jsx
@@ -1,45 +1,51 @@
-import React, {useState, useEffect, Fragment} from "react";
+import React, { Fragment, useEffect, useRef, useState } from "react";
+import "../css/bootstrap.min.css";
 import {db} from "../firebaseConfig.js";
-import {collection, getDocs, addDoc, deleteDoc, doc, updateDoc, onSnapshot} from "firebase/firestore";
+import {collection, onSnapshot, addDoc} from "firebase/firestore";
 
 export function Usuario(){
     const [usuarios, setUsuarios] = useState([]);
     const [nuevoUsuario, setNuevoUsuario] = useState("");
-    useEffect (()=>{
-        const usuariosRef = collection (db, "usuarios");
-        const unsubscribe = onSnapshot(usuariosRef, (snapshot) => {
+
+    useEffect(() => {
+        const usuariosRef = collection(db, "usuarios");
+        const unsuscribe = onSnapshot(usuariosRef, (snapshot) => {
             const lista = snapshot.docs.map((doc) => ({
                 id: doc.id,
-                ...doc.data()
+                ...doc.data(),
             }));
             setUsuarios(lista);
         });
-        return () => unsubscribe();
+        return () => unsuscribe();
     }, []);
 
     const agregarUsuario = async () => {
-        if(nuevoUsuario.trim() === "") return;
+        if (nuevoUsuario.trim() === "") return;
         await addDoc(collection(db, "usuarios"), {nombre: nuevoUsuario});
         setNuevoUsuario("");
-        alert("Usuario agregado");
-    }
-
-    return {
-        <Fragment>
-        <div className="container">
-            <h1 style={{textAlign: "center"}}>Lista de Usuarios</h1>
-            <hr/>
-
-            <div className="input-group">
-                <input
-                    value={nuevoUsuario}
-                    onChange={e => setNuevoUsuario(e.target.value)}
-                    className="form-control"
-                    type="text"
-                    placeholder="Ingrese el nombre del usuario"></input>
-                    <button onClick={agregarUsuario} className="btn btn-success">+</button>
-            </div>
-        </div>
-        </Fragment>
+        alert("Usuario agregado con éxito");
     };
+
+    return(
+        <Fragment>
+            <div className="container">
+                <h1 style={{textAlign: "center"}}>Lista de usuarios</h1>
+                <hr/>
+                <div className="input-group">
+                    <input
+                        value={nuevoUsuario}
+                        onChange={e => setNuevoUsuario(e.target.value)}
+                        className="form-control"
+                        type="text"
+                        placeholder="Ingrese nombre del usuario"></input>
+                    <button onClick={agregarUsuario} className="btn btn-success">+</button>
+                </div>
+                <ul className="list-group mt-2">
+                    {usuarios.map((usuario) => (
+                        <li key={usuario.id} className="list-group-item">{usuario.nombre}</li>
+                    ))}
+                </ul>
+            </div>
+        </Fragment>
+    )
 }
